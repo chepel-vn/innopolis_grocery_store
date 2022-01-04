dce8731bbaec5a133bba0ba477ca817493c4b53e
Valentin Chepel <chepel_vn@mail.ru>
2022-01-04 21:58:30 +0300

    add

--- conftest.py
+++ conftest.py
@@ -5,8 +5,9 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from fixtures.pages.application import Application
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
-logging.basicConfig(filename="grocery_store.log", level=logging.DEBUG)
+logging.basicConfig(filename="grocery_store.log", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
@@ -22,11 +23,13 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="session")
 def app(request):
     url = request.config.getoption("--url")
-    # s = Service(ChromeDriverManager().install())
+    s = Service(ChromeDriverManager().install())
     chrome_options = Options()
     chrome_options.headless = True
-    # driver = webdriver.Chrome(service=s)
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    chrome_options.add_argument("--kiosk")
+    driver = webdriver.Chrome(service=s)
+    # driver = webdriver.Chrome(ChromeDriverManager().install())
+    # driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
     logger.info(f"Start app on {url}.")
     app = Application(driver, url)

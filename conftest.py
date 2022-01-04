74601246db9e9985b6f951ea1f9f0c8711b4dbf8
Valentin Chepel <chepel_vn@mail.ru>
2022-01-04 19:16:48 +0300

    Add Chrome's headless mode

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from fixtures.pages.application import Application
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 logging.basicConfig(filename="grocery_store.log", level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -23,6 +24,8 @@ def pytest_addoption(parser):
 def app(request):
     url = request.config.getoption("--url")
     s = Service(ChromeDriverManager().install())
+    chrome_options = Options()
+    chrome_options.headless = True
     driver = webdriver.Chrome(service=s)
     # driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.maximize_window()
